3b3a7da7a638600941894d63c19ed40a094f2276
sebas-94 <sebas.solis.94@gmail.com>
2020-10-23 19:36:14 +0200

    add enviroment

---
+++ src/assets/enviroments/enviroment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyBdKidgqcZ5kgtPjTASsIXnrSJ1BABus7s",
+      authDomain: "proyectodaw-e02e1.firebaseapp.com",
+      databaseURL: "https://proyectodaw-e02e1.firebaseio.com",
+      projectId: "proyectodaw-e02e1",
+      storageBucket: "proyectodaw-e02e1.appspot.com",
+      messagingSenderId: "312000383839",
+      appId: "1:312000383839:web:7ad76e72edd7ee9bd784ea",
+      measurementId: "G-XTTBZNGWHH"
+    }
+  };
